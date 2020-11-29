4a373dcb57cb1638d9416579146daf2f438cae33
Yebume <45921728+yebume@users.noreply.github.com>
2020-11-29 15:00:01 +0100

    Update szuperprogram.c

--- szuperprogram.c
+++ szuperprogram.c
@@ -6,6 +6,7 @@
 typedef enum MUFALY {Action,Adventure,Comedy,Drama,Slice_of_Life,Fantasy,Magic,Supernatural,Horror,Mystery,Psychological,Romance,SciFi}MUFALY;
 typedef enum EVSZAK {SPRING,SUMMER,FALL,WINTER}EVSZAK;
 typedef enum ALAP   {Light_novel,Visual_novel,Manga,Muisc}ALAP;
+typedef enum COMMAND {ADD = 1,DEL,EDIT,LOGIN,EXIT}COMMAND;       //0, 1, 2, 3, 4
 
 
 
@@ -44,7 +45,7 @@ typedef struct User
 void push_anime(Anime NEW, Anime *tail)
 {
     Anime *newp = (Anime*)malloc(sizeof(Anime));
-    newp ->next = tail ->prev ->next;
+    newp ->next = tail;
     newp ->prev = tail ->prev;
     tail ->prev ->next = newp;
     tail ->prev = newp;
@@ -61,8 +62,12 @@ void push_anime(Anime NEW, Anime *tail)
 
 void anime2list(Anime **animes_head, Anime **animes_tail)
 {
-    FILE *fp =  fopen("NAGYHAZI/animek.txt", "r");
-    if (fp == NULL) return;
+    FILE *fpa =  fopen("NAGYHAZI/animek.txt", "r");
+    if (fpa == NULL)
+    {
+        printf("File open error\n");
+        return;
+    } 
     Anime *head = (Anime*)malloc(sizeof(Anime));
     Anime *tail = (Anime*)malloc(sizeof(Anime));
     head ->next = tail;
@@ -70,15 +75,18 @@ void anime2list(Anime **animes_head, Anime **animes_tail)
     tail ->next = NULL;
     head ->prev = NULL;
     Anime NEW;
-    while(fscanf(fp,"#%d,%[^,],%[^,],%d,%d,%d,%d,%d",&NEW.ID, NEW.name, NEW.studio, &NEW.genre, &NEW.base, &NEW.year, &NEW.season, &NEW.EP)==8)
+    while(fscanf(fpa,"#%d,%[^,],%[^,],%d,%d,%d,%d,%d",&NEW.ID, NEW.name, NEW.studio, &NEW.genre, &NEW.base, &NEW.year, &NEW.season, &NEW.EP)==8)
     {
         push_anime(NEW, tail);
     }
     *animes_head = head;
     *animes_tail = tail;
+        if (fclose(fpa) != 0)
+    {
+        printf("File close error\n");
+        return;
+    }
 }
-
-/*-------------------------------------------------------------------------------------------------------------------------*/
 
 void fesulista(User_data elemek, User_data *datahead, User_data *datatail)
 {
@@ -93,8 +101,6 @@ void fesulista(User_data elemek, User_data *datahead, User_data *datatail)
     newp ->seen = elemek.seen;
 }
 
-
-
 User *push_user(User UNEW, User *utail) //működnie kellene
 {
     User *newp = (User*)malloc(sizeof(User));
@@ -108,13 +114,14 @@ User *push_user(User UNEW, User *utail) //működnie kellene
     return newp;
 }
 
-
-
-
 void userdata2list(User **users_head, User **users_tail)
 {
     FILE *fp =  fopen("NAGYHAZI/profilok.txt", "r");
-    if (fp == NULL) return;
+    if (fp == NULL)
+    {
+        printf("File open error\n");
+        return;
+    } 
 
     User *head = (User*)malloc(sizeof(User));
     User *tail = (User*)malloc(sizeof(User));
@@ -125,9 +132,8 @@ void userdata2list(User **users_head, User **users_tail)
     head ->prev = NULL;
 
     User NEW;
-    User_data elemek;
 
-    while(fscanf(fp,"\n%[^,]", NEW.USER)==1)
+    while(fscanf(fp,"%[^,]", NEW.USER)==1)
     {
         User *curr = push_user(NEW, tail);
 
@@ -141,11 +147,12 @@ void userdata2list(User **users_head, User **users_tail)
         datatail ->next = NULL;
 
         curr->datas = datahead;
-
-        while (fscanf(fp,",#%d,%d",&elemek.seen, &elemek.ID)==2)
+        User_data elemek;
+        while (fscanf(fp,",#%d,%d",&elemek.ID, &elemek.seen)==2)
         {
             fesulista(elemek, datahead, datatail);
         }
+        fscanf(fp,"\n");
     }
     *users_head = head;
     *users_tail = tail;
@@ -156,12 +163,12 @@ void userdata2list(User **users_head, User **users_tail)
     }
 }
 
-/*-------------------------------------------------------------------------------------------------------------------------*/
+
 
 
 void users_kilistazasa(User *users_head)
 {
-    printf("Létező profilok:\n");
+    printf("\nLétező profilok:\n\n");
     User *curr = users_head ->next;
 
     while(curr ->next != NULL)                                                       
@@ -171,54 +178,105 @@ void users_kilistazasa(User *users_head)
     }
 }
 
-
-
-void user_add()
+int user_valid(User *users_head, char nevbuffer[])
 {
-    printf("Mi legyen a felhasználó neved?\n(maximum 25 karakter és vessző nem lehet benne)\n");
+    User *curr = users_head ->next;
+    while (curr ->next !=NULL)
+    {
+        if(strcmp(nevbuffer, curr->USER)==0) return 1;
+        curr == curr ->next;
+    }
+    return 0;
+}
+
+//void anime_pop();
+
+void pop_user(User *users_head, char nevbuffer[])
+{
+    User *curr = users_head->next;
+    
+    while (strcmp(nevbuffer, curr ->USER)==0)
+    {
+        curr = curr ->next;
+    }
+    
+    curr ->prev ->next = curr -> next;
+    curr ->next ->prev = curr ->prev;
+    //anime_pop();
+    free(curr);
+}
+
+
+
+void user_add(User *users_head)
+{
+    printf("\nMi legyen a felhasználó neved?\n(maximum 25 karakter és vessző nem lehet benne)\n");
     char nev[26];
     scanf("%s", nev);
 }
 
-void user_del()
+void user_del(User *users_head)
 {
-
-}
-
-void user_edit()
-{
-
-}
-
-void user_login()
-{
-
-}
-
-
-void user_menu(void)
-{
-    typedef enum COMMAND {add,del,edit,login,exit}COMMAND;       //0, 1, 2, 3, 4
-    printf("Mit szeretnél csinálni? (add, del, edit, login, exit)");
-    COMMAND mi = -1;
-    while (mi!=0 || mi!=1 || mi!=2 || mi!=3 || mi!=4)
+    printf("\nMelyik felhasznlót szeretnéd törölni (BACK)?\n");
+    users_kilistazasa(users_head);
+    char nevbuffer[26];
+    scanf("%s", nevbuffer);
+    if (user_valid(users_head, nevbuffer) == 0) 
     {
-        scanf("%d", &mi);
-        if (mi!=0 || mi!=1 || mi!=2 || mi!=3 || mi!=4)
-            printf("Kérlek írj be valid kommandot");
+        printf("Nem valid felhasználó!");
+        return;
     }
-
-    switch (mi)
+    printf("\nBiztos? (y/n)\n");
+    char sure = '0';
+    while(sure !='n' || sure != 'y')
     {
-        case add: user_add(); break;
-        case del: user_del(); break;
-        case edit: user_edit(); break;
-        case login: user_login(); break;
-        case exit: return;
-        default: break;
+        scanf("%c", &sure);
     }
+    if (sure == 'n') return;
+    if (sure == 'y') pop_user(users_head, nevbuffer);
 }
 
+void user_edit(User *users_head)
+{
+    users_kilistazasa(users_head);
+}
+
+void user_login(User *users_head)
+{
+    users_kilistazasa(users_head);
+}
+
+
+
+
+void print_menu(void) {
+    printf("\nMit szeretnél csinálni?\n\n");
+    printf("\t%d. Felhasználó létrehozása\n", ADD);
+    printf("\t%d. Felhasználó törlése\n", DEL);
+    printf("\t%d. Felhasználónév módosítása\n", EDIT);
+    printf("\t%d. Felhasználó létrehozása\n", LOGIN);
+    printf("\t%d. Kilépés\n", EXIT);
+}
+
+void user_menu(User *users_head)
+{
+int selection = 0;
+    while (selection != EXIT)
+    {
+        print_menu();
+        scanf("%d", &selection);
+
+        switch (selection)
+        {
+            case ADD: user_add(users_head); break;
+            case DEL: user_del(users_head); break;
+            case EDIT: user_edit(users_head); break;
+            case LOGIN: user_login(users_head); break;
+            case EXIT: printf("Viszlát!"); return;
+            default:printf("Unknown option.\n"); break;
+        }
+    }
+}
 
 
 int main(void)
@@ -231,8 +289,7 @@ int main(void)
     Anime *animes_tail;
     anime2list(&animes_head, &animes_tail);
 
-    users_kilistazasa(users_head);
-
-    //user_menu();
+    //user_menu(users_head);
+    printf("\n\n%s %d %d", users_head->next->USER, users_head->next->datas->next->ID, users_head->next->datas->next->seen);
     return 0;
 }
